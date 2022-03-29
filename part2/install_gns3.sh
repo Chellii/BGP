e1bc57d90d2aea32114e844318c4eb72a8639c87
Chaimae El oiriagli <cel-oiri@e2r12p3.1337.ma>
2022-03-29 15:15:29 +0000

    part 2

---
+++ part2/install_gns3.sh
@@ -0,0 +1,30 @@
+#### BGP ####
+sudo add-apt-repository ppa:gns3/ppa -y
+sudo apt update                                
+sudo apt install -y gns3-gui gns3-server
+#### Install IOU Support ####
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y gns3-iou
+#### Install Docker ####
+sudo apt remove docker docker-engine docker.io
+sudo apt install -y curl
+sudo apt-get install apt-transport-https ca-certificates curl \ software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG sudo chelli
+sudo usermod -aG docker chelli
+sudo usermod -aG ubridge chelli
+sudo usermod -aG libvirt chelli
+sudo usermod -aG wireshark chelli
+sudo usermod -aG kvm chelli
+sudo chmod +x /usr/bin/ubridge
+newgrp docker 
+sudo chmod +x /usr/bin/dumpcap
+# chmod 777 scripts.sh
+# sudo apt install -y openssh-server
+# scp scripts.sh misaki@10.12.100.28:~
